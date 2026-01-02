e9cb03c7a529df65314679fc83d27fa3d40d0acc
joaokranio <jktechinformatica@gmail.com>
2026-01-02 16:54:08 -0300

    test: organiza cenários de tarefas por domínio e adiciona fluxo de atualização

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -19,6 +19,11 @@ export class TaskPages {
         await this.page.click('css=button >> text=Create')
     }
 
+    async toggle(taskName: string) {
+        const target = this.page.locator(`xpath=//p[text()="${taskName}"]/..//button[contains(@class,"Toggle")]`)
+        await target.click()
+    }
+
     async shouldHaveText(taskName: string) {
         const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
         await expect(target).toBeVisible()
@@ -27,5 +32,10 @@ export class TaskPages {
     async alertHaveText(text: string) {
         const target = this.page.locator('.swal2-html-container')
         await expect(target).toHaveText(text)
+    }
+
+    async shouldBeDone(taskName: string) {
+        const target = this.page.getByAltText(taskName)
+        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
     }
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -9,42 +9,61 @@ test.beforeEach(async ({ page }) => {
     await tasksPages.go()
 })
 
-test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
-    // Dado que eu tenho uma nova tarefa
-    const task = data.success as TaskModel
-    await deleteTaskByHelper(request, task.name)
+test.describe('cadastro', () => {
+    test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+        // Dado que eu tenho uma nova tarefa
+        const task = data.success as TaskModel
+        await deleteTaskByHelper(request, task.name)
 
-    // Quando faço o cadastro dessa tarefa
-    const tasksPages: TaskPages = new TaskPages(page)
-    await tasksPages.create(task)
+        // Quando faço o cadastro dessa tarefa
+        const tasksPages: TaskPages = new TaskPages(page)
+        await tasksPages.create(task)
 
-    // Então essa tarefa deve ser exibida na lista
-    await tasksPages.shouldHaveText(task.name)
+        // Então essa tarefa deve ser exibida na lista
+        await tasksPages.shouldHaveText(task.name)
+    })
+
+    test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+        // Dado que eu tenho uma nova tarefa 
+        const task = data.duplicate as TaskModel
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
+
+        // Quando tento cadastrar novamente a mesma tarefa
+        const tasksPages: TaskPages = new TaskPages(page)
+        await tasksPages.create(task)
+
+        // Então deverá ser exibido um pop-up informando que a tarefa já foi cadastrada 
+        await tasksPages.alertHaveText('Task already exists!')
+    })
+
+    test('campo obrigatório', async ({ page }) => {
+        // Dado que eu não tenho informações de uma tarefa
+        const task = data.required as TaskModel
+
+        // Quando tento realizar o cadastro da tarefa sem passar as informações.
+        const tasksPages: TaskPages = new TaskPages(page)
+        await tasksPages.create(task)
+
+        // Então o sistema me informa que não é possível realizar essa ação.
+        const validationMessage = await tasksPages.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+        expect(validationMessage).toEqual('This is a required field')
+    })
+})
+test.describe('atualização', () => {
+    test('Deve concluir uma tarefa', async ({ page, request }) => {
+        //Dada que eu executo uma tarefa
+        const task = data.update as TaskModel
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
+
+        // Quando eu a marco como finalizada.
+        const tasksPages: TaskPages = new TaskPages(page)
+        await tasksPages.toggle(task.name)
+
+        // Então o sistema deverá concluir a tarefa
+        await tasksPages.shouldBeDone(task.name)
+    })
 })
 
-test('não deve permitir tarefa duplicada', async ({ page, request }) => {
-    // Dado que eu tenho uma nova tarefa 
-    const task = data.duplicate as TaskModel
-    await deleteTaskByHelper(request, task.name)
-    await postTask(request, task)
 
-    // Quando tento cadastrar novamente a mesma tarefa
-    const tasksPages: TaskPages = new TaskPages(page)
-    await tasksPages.create(task)
-
-    // Então deverá ser exibido um pop-up informando que a tarefa já foi cadastrada 
-    await tasksPages.alertHaveText('Task already exists!')
-})
-
-test('campo obrigatório', async ({ page }) => {
-    // Dado que eu não tenho informações de uma tarefa
-    const task = data.required as TaskModel
-
-    // Quando tento realizar o cadastro da tarefa sem passar as informações.
-    const tasksPages: TaskPages = new TaskPages(page)
-    await tasksPages.create(task)
-
-    // Então o sistema me informa que não é possível realizar essa ação.
-    const validationMessage = await tasksPages.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
-    expect(validationMessage).toEqual('This is a required field')
-})
